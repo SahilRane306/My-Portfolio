edbce31ba032d6c0fca4db40eb900fbd81f55c99
SahilRane306 <sahilrajeshrane@gmail.com>
2025-09-25 12:48:01 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Responsive menu toggle
+const toggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('nav ul');
+
+toggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
